a42356f4d9144d3786bb45758fed25ec889a869f
黄有凡 <919101209@qq.com>
2020-09-20 15:27:44 +0800

    在"master"上提交

--- js/tab.js
+++ js/tab.js
@@ -6,10 +6,16 @@ class Tab {
         this.main = document.querySelector(id);
         this.lis = this.main.querySelectorAll('li');
         this.sections = this.main.querySelectorAll('section');
+        //先获取加号
+        this.add = this.main.querySelector('.tabadd');
+        //再获取ul
+        this.ul = this.main.querySelector('.firstnav ul:first-child');
         this.init();
     }
     //初始化绑定事件
     init() {
+        //这里的this指的是实例对象,给加号添加点击事件
+        this.add.onclick = this.addTab;
         for (var i = 0; i < this.lis.length; i++) {
             //给每个li添加属性,添加索引号,把li当做对象
             this.lis[i].index = i;
@@ -31,6 +37,11 @@ class Tab {
             this.lis[i].className = '';
             this.sections[i].className = '';
         }
+    }
+    addTab() {
+        var li = '<li class="liactive"><span>新选项卡</span><span class="iconfont icon-guanbi"></span></li>';
+        //把新创建的li添加进ul
+        that.ul.insertAdjacentHTML('beforeend', li);
     }
 }
 new Tab('#tab');
